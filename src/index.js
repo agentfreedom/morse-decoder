6138fe1cb4cc8f9c8e9a7fb3a17f251a9da10c14
agentfreedom <agentfreedom@zoho.com>
2022-01-28 20:02:53 +0200

    feat: add solution for task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,32 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let valueArray = [];
+    for (let i = 0; i < expr.length; i += 10) {
+      const startsWithOne = expr.slice(i, i + 10);
+      const part = startsWithOne.slice(startsWithOne.indexOf("1"));
+      let res = "";
+      for (let i = 0; i < part.length; i += 2) {
+          const a = part.slice(i, i + 2);
+          switch (a) {
+              case "10":
+                  res += ".";
+                  break;
+              case "11":
+                  res += "-";
+                  break;
+              case "*":
+                  res += " ";
+                  break;
+          }
+      }
+      valueArray.push(res);
+    }
+
+    return valueArray
+      .map((item) => (item === " " ? " " : MORSE_TABLE[item]))
+      .join("");
+    
 }
 
 module.exports = {
